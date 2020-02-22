f57791aed04f80c8bfe2b24164fb9c42b2431263
i <ivan-cheglakov@yandex.ru>
2020-02-22 22:33:16 +0300

    1. Added cardNumber masking 2. Added Notification generator

--- src/main/kotlin/ru/tinkoff/fintech/model/NotificationMessageInfo.kt
+++ src/main/kotlin/ru/tinkoff/fintech/model/NotificationMessageInfo.kt
@@ -3,10 +3,10 @@ package ru.tinkoff.fintech.model
 import java.time.LocalDateTime
 
 data class NotificationMessageInfo(
-    val name: String,
-    val cardNumber: String,
-    val cashback: Double,
-    val transactionSum: Double,
-    val category: String,
-    val transactionDate: LocalDateTime
+    val name: String = "",
+    val cardNumber: String = "",
+    val cashback: Double = 0.0,
+    val transactionSum: Double = 0.0,
+    val category: String = "",
+    val transactionDate: LocalDateTime = LocalDateTime.now()
 )
--- src/main/kotlin/ru/tinkoff/fintech/service/notification/NotificationMessageGeneratorImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/notification/NotificationMessageGeneratorImpl.kt
@@ -1,12 +1,32 @@
 package ru.tinkoff.fintech.service.notification
 
+import freemarker.template.Configuration
+import freemarker.template.Template
 import ru.tinkoff.fintech.model.NotificationMessageInfo
+import java.io.ByteArrayOutputStream
+import java.io.OutputStreamWriter
+import java.io.Writer
+
+private const val TEMPLATES_PATH = "/templates/"
+private const val CASH_TEMPLATE_FILENAME = "cashback.txt.ftl"
+private const val TEMPLATE_PARAMETER_KEY_CARD_NUMBER = "maskCardNumber"
+private const val TEMPLATE_PARAMETER_KEY_NOTIFICATION_INFO = "notificationMessageInfo"
 
 class NotificationMessageGeneratorImpl(
     private val cardNumberMasker: CardNumberMasker
 ) : NotificationMessageGenerator {
 
     override fun generateMessage(notificationMessageInfo: NotificationMessageInfo): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val cfg = Configuration(Configuration.VERSION_2_3_0)
+        cfg.setClassForTemplateLoading(NotificationMessageGeneratorImpl::class.java, TEMPLATES_PATH)
+        val template: Template = cfg.getTemplate(CASH_TEMPLATE_FILENAME)
+        val input: MutableMap<String, Any> = HashMap()
+        input[TEMPLATE_PARAMETER_KEY_NOTIFICATION_INFO] = notificationMessageInfo
+        input[TEMPLATE_PARAMETER_KEY_CARD_NUMBER] = cardNumberMasker.mask(notificationMessageInfo.cardNumber)
+
+        val mergedTemplate = ByteArrayOutputStream()
+        val output: Writer = OutputStreamWriter(mergedTemplate)
+        template.process(input, output)
+        return mergedTemplate.toString().trimIndent()
     }
 }
