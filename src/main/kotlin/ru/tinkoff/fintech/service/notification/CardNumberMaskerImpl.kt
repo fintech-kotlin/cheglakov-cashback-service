16d78ba41cf42d91f17b2faec011868b3f23ae82
i <ivan-cheglakov@yandex.ru>
2020-02-25 19:58:11 +0300

    Fix

--- src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
@@ -3,7 +3,7 @@ package ru.tinkoff.fintech.service.notification
 class CardNumberMaskerImpl: CardNumberMasker {
 
     override fun mask(cardNumber: String, maskChar: Char, start: Int, end: Int): String {
-        if (cardNumber === "") return ""
+        if (cardNumber.isEmpty()) return ""
         if (start > end) throw Exception("Start index cannot be greater than end index")
         if (cardNumber.length < end) return "################"
         return cardNumber.replaceRange(start, end, maskChar.toString().repeat(end - start))
